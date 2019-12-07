5b36a77bc92f5d2325dbab529cb653f45a0bc7b2
Katrina_Dierking <kalynndier@gmail.com>
2019-12-07 00:17:35 -0500

    snapshot test passed - of course

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+test ('Dashboard renders correctly', () => {
+    const dashboard = render (<Dashboard />)
+    expect (dashboard).toMatchSnapshot();
+})
+
