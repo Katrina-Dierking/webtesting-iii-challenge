6f8490a8a8ef200b8e9a8979bc4897f0f69dccf0
Katrina_Dierking <kalynndier@gmail.com>
2019-12-07 22:31:58 -0500

    control tests defined. Now to set up

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,7 +3,16 @@ import {render, fireEvent} from '@testing-library/react';
 
 import Controls from './Controls';
 
+// snapshot test
 test ('renders correctly', () => {
     expect (render(<Controls />)).toMatchSnapshot();
-})
+});
+// end snapshot test
+
+test ('gate opens when clicked', () => {});
+test ('gate closes when clicked', () => {});
+test ('button says open when clicked', () => {});
+test ('button says closed when clicked', () => {});
+test ('closed button disabled when gate is locked', () => {});
+test ('locked button is disabled when gate is open', () => {});
 
