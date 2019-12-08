f171df01635c7fea3ae81f220e964f73ffed40c9
Katrina_Dierking <kalynndier@gmail.com>
2019-12-07 20:31:11 -0500

    display tests defined. Now setting up

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,8 +4,22 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
+
 test ('Dashboard renders correctly', () => {
     const dashboard = render (<Dashboard />)
     expect (dashboard).toMatchSnapshot();
 })
 
+
+
+test ('Mocking Display', () => {
+    const displayMock = jest.fn();
+    displayMock('displayed');
+    expect(displayMock).toHaveBeenCalled();
+})
+
+test ('Mocking Controls', () => {
+    const controlsMock = jest.fn();
+    controlsMock('controlled');
+    expect(controlsMock).toHaveBeenCalled();
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Display from './Display';
+
+test('renders correctly', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+})
+
+test ('displays if gate is open/closed', () => {})
+test ('displays if gate is locked/unlocked', () => {})
+test ('displays Closed if the closed prop is true', () => {})
+test ('displays Open if closed prop is false', () => {})
+test ('displays Locked if locked prop is true', () => {})
+test ('displays Unlocked if locked prop is false', () => {})
+test ('gate is green if open and unlocked', () => {})
+test ('gate is red if closed and locked', () => {})
+
